41bb592e0e97cfaaa9f39ad554e7f898812d969d
SaVyrin <sarajkinaa@gmail.com>
2022-05-29 21:50:35 +0300

    Added Chapter 16 Tasks example

---
+++ Chapter16.Tasks/src/main/java/com/hfad/chapter16tasks/TaskDiffItemCallback.kt
@@ -0,0 +1,10 @@
+package com.hfad.chapter16tasks
+
+import androidx.recyclerview.widget.DiffUtil
+
+class TaskDiffItemCallback : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean =
+        (oldItem.taskId == newItem.taskId)
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean = (oldItem == newItem)
+}
---
+++ Chapter16.Tasks/src/main/java/com/hfad/chapter16tasks/TasksFragment.kt
@@ -0,0 +1,49 @@
+package com.hfad.chapter16tasks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.hfad.chapter16tasks.databinding.FragmentTasksBinding
+
+class TasksFragment : Fragment() {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModelFactory: TasksViewModelFactory
+    private val viewModel: TasksViewModel by viewModels { viewModelFactory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+        viewModelFactory = TasksViewModelFactory(dao)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = TaskItemAdapter()
+        binding.tasksList.adapter = adapter
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer { tasks ->
+            tasks?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
